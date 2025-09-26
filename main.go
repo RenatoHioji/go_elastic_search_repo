b68a8eab1de7723608474ed40895c7ed7b7fb147
DSM6 <fatec-dsm6@academico.intra>
2025-09-26 20:06:25 -0300

    feat: adicionando configuracao do elasticsearch

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"strings"
 
+	"github.com/RenatoHioji/go_elastic_search_repo/model"
 	"github.com/elastic/go-elasticsearch/v9"
 )
 
 func main() {
-	es, _ := elasticsearch.NewDefaultClient()
-
+	//es, _ := elasticsearch.NewDefaultClient()
+	cfg := elasticsearch.Config{
+		Addresses: []string{"http://localhost:9200"},
+		// Password: password...
+		// Username: username...
+	}
+	es, _ := elasticsearch.NewClient(cfg)
 	log.Println(es.Info())
+	document := model.Document{Name: "go-elastic-search"}
+
+	data, _ := json.Marshal(document)
+	es.Index("my_index", bytes.NewReader(data))
+
+	query := `{ "query: { "match_all": {} } }"`
+
+	es.Search(
+		es.Search.WithIndex("my_index"),
+		es.Search.WithQuery(query),
+	)
+	es.Update("my_index", "id", strings.NewReader(`{doc: {language: "Go"}}`))
+	es.Delete("my_index", "id")
+
+	es.Indices.Delete([]string{"my_index"})
+
 }
---
+++ model/document.go
@@ -0,0 +1,5 @@
+package model
+
+type Document struct {
+	Name string `json:"name"`
+}
