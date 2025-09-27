272898856aaeea994e39a928b4d13489f62c17bc
RenatoHioji <okamoto.hioji@gmail.com>
2025-09-27 16:21:51 -0300

    feat: alocando main.go para a pasta de servers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/RenatoHioji/go_elastic_search_repo/internal/app"
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+	db := config.InitDB(cfg)
 
+	app.NewApp(db)
 	config.InitES(cfg)
 	config.InitRoutes(cfg)
 }
