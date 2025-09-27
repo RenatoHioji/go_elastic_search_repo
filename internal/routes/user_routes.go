89884e405d40d91675b9dba55cb9bd4a550c6a70
RenatoHioji <okamoto.hioji@gmail.com>
2025-09-27 16:22:58 -0300

    feat: adicionando caminho de user

---
+++ internal/routes/user_routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/RenatoHioji/go_elastic_search_repo/internal/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitUserRoutes(r *gin.Engine) {
+	group := r.Group("/users")
+	{
+		group.GET("/", handlers.GetUsers)
+	}
+}
