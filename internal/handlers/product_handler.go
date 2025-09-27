029edee659dccdb02b9947a6486b06f9ccc0c5dd
RenatoHioji <okamoto.hioji@gmail.com>
2025-09-27 16:22:17 -0300

    feat: adicionando caminho de produtos

---
+++ internal/handlers/product_handler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello From products",
+	})
+}
