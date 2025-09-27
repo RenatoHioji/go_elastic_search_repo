abb292209f7a697b85315733b80d36e04d940d12
RenatoHioji <okamoto.hioji@gmail.com>
2025-09-27 16:29:24 -0300

    feat: adicionando lib de shopspring

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,4 +1,10 @@
 package models
 
+import "github.com/shopspring/decimal"
+
 type Product struct {
+	ID          int             `json:"id"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Price       decimal.Decimal `json:"price"`
 }
