52b0b78a292b82f1b7cfebf652ef03b8a3b4f351
RenatoHioji <okamoto.hioji@gmail.com>
2025-09-29 14:04:12 -0300

    feat: adding Search to App struct

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,11 +3,13 @@ package app
 import (
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/config"
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/product"
+	"github.com/RenatoHioji/go_elastic_search_repo/internal/search"
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/user"
 )
 
 type App struct {
 	ProductHandler *product.ProductHandler
 	UserHandler    *user.UserHandler
+	SearchHandler  *search.SearchHandler
 	Config         config.Config
 }
