b513612453230a7a73f4325c8de20505f1bb0574
RenatoHioji <okamoto.hioji@gmail.com>
2025-09-29 14:05:01 -0300

    refactor: adjusting route conf to route init

--- internal/routes/route_init.go
+++ internal/routes/route_init.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/app"
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/product"
+	"github.com/RenatoHioji/go_elastic_search_repo/internal/search"
 	"github.com/RenatoHioji/go_elastic_search_repo/internal/user"
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +15,7 @@ func InitRoutes(app *app.App) {
 
 	product.InitProductRoutes(r, app.ProductHandler)
 	user.InitUserRoutes(r, app.UserHandler)
+	search.InitApiRoutes(r, app.SearchHandler)
 
 	if err := r.Run(app.Config.ServerUrl); err != nil {
 		log.Fatal(err)
